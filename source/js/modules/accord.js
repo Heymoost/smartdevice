b8728a782ab88df0529cc00db0dcda38fac91ce7
AntonSuslov <87482005+Heymoost@users.noreply.github.com>
2022-09-02 19:07:54 +0400

    Правки код ревью 2

--- source/js/modules/accord.js
+++ source/js/modules/accord.js
@@ -1,49 +1,19 @@
 let link = document.querySelector('.link');
-let linkBtn = document.querySelector('.link__button');
 let contacts = document.querySelector('.contacts');
-let contactsBtn = document.querySelector('.contacts__button');
+
 
 link.classList.remove('link--closed--nojs');
 contacts.classList.remove('contacts--closed--nojs');
+link.classList.add('accord--closed');
+contacts.classList.add('accord--closed');
 
-function openLink() {
-  if (contacts.classList.contains('contacts--oppened')) {
-    contacts.classList.add('contacts--closed');
-    contacts.classList.remove('contacts--oppened');
-    link.classList.remove('link--closed');
-    link.classList.add('link--oppened');
-  } else {
-    if (link.classList.contains('link--oppened')) {
-      link.classList.remove('link--oppened');
-      link.classList.add('link--closed');
-    } else {
-      link.classList.add('link--oppened');
-      link.classList.remove('link--closed');
-    }
-  }
-}
-
-function openContacts() {
-  if (link.classList.contains('link--oppened')) {
-    link.classList.add('link--closed');
-    link.classList.remove('link--oppened');
-    contacts.classList.remove('contacts--closed');
-    contacts.classList.add('contacts--oppened');
-  } else {
-    if (contacts.classList.contains('contacts--oppened')) {
-      contacts.classList.remove('contacts--oppened');
-      contacts.classList.add('contacts--closed');
-    } else {
-      contacts.classList.add('contacts--oppened');
-      contacts.classList.remove('contacts--closed');
-    }
-  }
-}
-
-linkBtn.addEventListener('click', () => {
-  openLink();
-});
-
-contactsBtn.addEventListener('click', () => {
-  openContacts();
+document.querySelectorAll('.btn').forEach(function (target) {
+  target.addEventListener('click', function (event) {
+    document.querySelectorAll('.accord').forEach(function (tab) {
+      tab.classList.remove('accord--oppened');
+      tab.classList.add('accord--closed');
+    });
+    event.target.closest('.accord').classList.add('accord--oppened');
+    event.target.closest('.accord').classList.remove('accord--closed');
+  });
 });
